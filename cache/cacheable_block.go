649d614556c1b96fa3ab5fd71934160b4f1d414f
wakiyamap <wakiyamap@gmail.com>
2021-05-19 01:26:51 +0900

    Fix type(CacheableBlock)

--- cache/cacheable_block.go
+++ cache/cacheable_block.go
@@ -5,7 +5,7 @@ import "github.com/btcsuite/btcutil"
 // CacheableBlock is a wrapper around the btcutil.Block type which provides a
 // Size method used by the cache to target certain memory usage.
 type CacheableBlock struct {
-	Block *btcutil.Block
+	*btcutil.Block
 }
 
 // Size returns size of this block in bytes.
